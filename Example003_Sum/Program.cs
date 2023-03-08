398811ac31bed4d45290e7eb8172feaeb8d81484
Svetlana <khabarova09@rambler.ru>
2023-03-08 17:58:23 +0300

    Example003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// int numberA=3;
+// int numberB=8;
+// Console.WriteLine(numberA + numberB);
+
+
+// int numberA=22;
+// int numberB=9;
+// int result=numberA + numberB;
+// Console.WriteLine(result);
