8e9e620d7c7bfcd2ff0bcbf42e7299e612877c26
Svetlana <khabarova09@rambler.ru>
2023-03-09 12:00:49 +0300

    Example005_ConditionalIfElse

---
+++ Example005_ConditionalIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")  //ToLower() и текст в кавычках в НИЖНЕМ регистре
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
