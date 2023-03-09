492cf145b39b723c05ab0c5b4e893aee18ba77a5
Svetlana <khabarova09@rambler.ru>
2023-03-09 09:53:30 +0300

    Example004_Div1

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,10 @@
-﻿double name1=12;
-double name2=5;
-Console.WriteLine(name1 / name2);
+﻿// double name1=12;
+// double name2=5;
+// Console.WriteLine(name1 / name2);
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
